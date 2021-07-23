be4a9f61565e1a3a4c155d8ad12cbc4b81c0fe93
Jozef Mrazek <jmrazek@redhat.com>
2021-07-23 13:24:12 +0200

    Hide endpoint from api

--- src/main/java/org/jboss/pnc/uilogger/rest/Root.java
+++ src/main/java/org/jboss/pnc/uilogger/rest/Root.java
@@ -1,6 +1,7 @@
 package org.jboss.pnc.uilogger.rest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,6 +10,7 @@ import javax.ws.rs.Path;
 @Path("/")
 public class Root {
 
+    @Operation(hidden = true)
     @GET
     public String wellcome() {
         return "Hey there.";
